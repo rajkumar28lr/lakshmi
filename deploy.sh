6559aba06c0d504f7bcb0b20e32d4d6d465d3c7b
rajkumar28lr <lrrajkumar28@gmail.com>
2024-10-29 15:48:49 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,14 +3,14 @@ if [[ $GIT_BRANCH == "origin/dev" ]]; then
     # Build your project
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u ar8888 -p dckr_pat_EADWHoJ3ZHIAazRONGu5WkNt0EY
-    docker tag test ar8888/dev
-    docker push ar8888/dev
+    docker login -u rajkumar2128 -p dckr_pat_Hx7I1qSP_Ol23ZeCgUbtnJ9PMAQ 
+    docker tag test rajkumar2128/dev
+    docker push rajkumar2128/dev
 
 elif [[ $GIT_BRANCH == "origin/main" ]]; then
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u ar8888 -p dckr_pat_EADWHoJ3ZHIAazRONGu5WkNt0EY
-    docker tag test ar8888/prod 
-    docker push ar8888/prod
+    docker login -u rajkumar2128 -p dckr_pat_Hx7I1qSP_Ol23ZeCgUbtnJ9PMAQ
+    docker tag test rajkumar2128/prod 
+    docker push rajkumar2128/prod
 fi
